3f2fcf4944fc9bce7da6a786aa516bbabd34562b
Sunday OYENIYI <sunday.oyeniyi@modelis-tech.com>
2019-04-24 23:54:11 +0000

    Ajout d'un nouveau module OneSignal pour les notifications

--- app.module.ts
+++ app.module.ts
@@ -6,6 +6,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { OneSignal } form '@ionic-native/one-signal';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -16,7 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    OneSignal
   ],
   bootstrap: [AppComponent]
 })
